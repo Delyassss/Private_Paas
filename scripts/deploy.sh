70c7249cbac2b5da8d63ad5448d819949216c033
Delyassss <ilyassda04@gmail.com>
2026-08-20 08:43:26 +0100

    this is so weird

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -44,9 +44,11 @@ if [[ "${YML_FILE}" =~ ^[[:space:]]*$ ]];
             docker run -d --restart unless-stopped --name ${app_name}_container  --network paas_net -e PORT=5000 ${app_name,,} #,, for lowecase
              cat << EOF | docker exec -i mypaas_nginx tee "/etc/nginx/conf.d/${app_name}.conf" > /dev/null # the second > to ignore the tee default stdout
 server {
+        listen 8081;
+        listen [::]:8081;
         listen 80;
         listen [::]:80;
-        server_name  ${app_name,,}.localhost;
+        server_name  ${app_name,,}.localhost:8081;
 
      location / {
         proxy_pass http://${app_name,,}:5000;
@@ -67,9 +69,11 @@ EOF
             # the second > to ignore the tee default stdout AND the terminating EOF must be at colum 0
             cat << EOF | docker exec -i mypaas_nginx tee "/etc/nginx/conf.d/${service_name}.conf" > /dev/null 
 server {
+    listen 8081;
+    listen [::]:8081;
     listen 80;
     listen [::]:80;
-    server_name  ${service_name}.localhost;
+    server_name  ${service_name}.localhost ;
     
     location /  {
         proxy_pass http://${service_name}:5000;
